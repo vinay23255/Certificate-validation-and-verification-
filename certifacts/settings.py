e416b02bbe7224755be3a7b1b432546577caefcd
vinay23255 <vinaythakkellapati@gmail.com>
2026-06-16 22:52:20 +0530

    Update settings.py

--- certifacts/settings.py
+++ certifacts/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-564e)nne#k#to4yui#0-sy%9$()^txu(bg+qqd9u!hge6$bs23'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".vercel.app",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
