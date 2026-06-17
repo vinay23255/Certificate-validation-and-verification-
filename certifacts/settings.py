c72753255610460e8a5bef20daab33e9966e05ea
vinay23255 <vinaythakkellapati@gmail.com>
2026-06-17 19:31:36 +0530

    Update settings.py

--- certifacts/settings.py
+++ certifacts/settings.py
@@ -127,3 +127,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
