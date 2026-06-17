89383d4972ab91409d058973993153d0204b69dd
vinay23255 <vinaythakkellapati@gmail.com>
2026-06-17 11:54:03 +0530

    Add error handling to build script
    Added error handling to terminate execution on failure.

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Upgrade pip and install required dependencies
+# Terminate execution if any step throws an unhandled exception
+set -e
+
+# Upgrade base setup and fetch standard project requirements
 python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 
-# Gather all static folder CSS assets into the /staticfiles directory
+# Execute Django's compilation algorithm to bundle assets cleanly
 python3 manage.py collectstatic --noinput --clear
